20cec6aa2d7c1a7df61038838b2dd5bbdf508cf0
driwn <dragunovskiyiwn@yandex.ru>
2024-03-17 13:04:51 +0500

    hgfd

---
+++ script.js
@@ -0,0 +1,52 @@
+const a = [1, 2, 3, 4, 5, 6, 7];
+
+let user = {
+  username: "asfadfs",
+  password: "asdasda",
+  post: [
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+    {
+      titlte: "jopd",
+      asdas: "sdfsd",
+    },
+  ],
+};
+
+const fn = (user) => {
+  console.log(user.username);
+};
+
+function sayMyName(user) {
+  console.log(user.username);
+}
+
+sayMyName(user);
+
+fn(user)
